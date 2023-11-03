0b5c28223e7fe9a676ac33fa197cf26bfd97d812
yasirumar22 <yasirumar22@gmail.com>
2023-11-03 15:30:22 +0530

    Day1

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,11 @@
+let js = 'amazing';
+if (js == 'amazing') alert('sounds fun');
+console.log(34 - 22 + 111)
+
+let firstName = "Martin";
+console.log(typeof firstName);
+let year;
+console.log(year)
+console.log(typeof year);
+
+console.log(typeof null);
